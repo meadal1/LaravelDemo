3010ce6835e25b19b2e1508d9029741b116c6b10
Aaron Mead (1000048565) <MEADAL1@student.op.ac.nz>
2019-08-15 16:11:13 +1200

    Hand in my laravel tutorial

---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+*/
+
+Route::get('/','PagesController@index')->name('pages.index');
+Route::get('/about','PagesController@about')->name('pages.about');
+
+Route::get('/todos','TodosController@index')->name('todos.index');
+Route::get('/todos/create','TodosController@create')->name('todos.create');
+Route::post('/todos','TodosController@store')->name('todos.store'); // making a post request
+Route::get('/todos/{id}','TodosController@show')->name('todos.show');
+Route::get('/todos/{id}/edit','TodosController@edit')->name('todos.edit');
+Route::put('/todos/{id}','TodosController@update')->name('todos.update'); // making a put request
+Route::delete('/todos/{id}','TodosController@destroy')->name('todos.destroy'); // making a delete request
